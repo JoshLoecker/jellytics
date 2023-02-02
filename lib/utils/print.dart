c73f61daee8e9df703de5b078d8a0e6e12a3f51f
Josh Loecker <joshloecker@icloud.com>
2023-02-01 22:05:43 -0600

    Major changes

---
+++ lib/utils/print.dart
@@ -0,0 +1,10 @@
+/*
+Create a function to print a string as a json object
+ */
+import 'dart:convert';
+
+void printAsJson(Map json) {
+  JsonEncoder encoder = const JsonEncoder.withIndent('  ');
+  String prettyprint = encoder.convert(json);
+  print(prettyprint);
+}
--- lib_old/views/item_detail_builder.dart
+++
@@ -1,2 +0,0 @@
-/// This file is responsible for buildling the "detail" view of items
-/// This will be used in library details and activity details
